b7f0d33bd46b87dab3bed8a918e8a6558de69bc4
Thomas Wimmer <thomaswimmer@posteo.com>
2024-04-25 20:49:28 +0200

    Reproduce Fig S1
    In a later change, I'll add support for two modes.

--- src/saltx/tests/reference/test_mohammed_2022_figs2.py
+++ src/saltx/tests/reference/test_mohammed_2022_figs2.py
@@ -106,7 +106,30 @@ def real_const(V, real_value: float) -> fem.Constant:
     return fem.Constant(V.mesh, complex(real_value, 0))
 
 
-def calculate_mode_and_intensity(system, D1: int, D2: int) -> tuple[float, float]:
+def calculate_mode_and_intensity(
+    system, D1: int, D2: int, initial_s: float
+) -> tuple[float, float, float]:
+    """Calculate the first laser mode.
+
+    Parameters
+    ----------
+    system
+    D1
+        pump strength in the left wedge
+    D2
+        pump strength in the right wedge
+    initial_s
+        initial guess of the parameter s (the amplitude) of the first mode
+
+    Returns
+    -------
+    fac
+        f*a/c of the mode
+    intens
+        Intensity of the mode
+    s
+        `mode.s`
+    """
     u = ufl.TrialFunction(system.V)
     v = ufl.TestFunction(system.V)
 
@@ -156,14 +179,14 @@ def calculate_mode_and_intensity(system, D1: int, D2: int) -> tuple[float, float
     newton_operators = newtils.create_multimode_solvers_and_matrices(nlp, max_nmodes=1)
 
     if evals.imag.max() < 1e-9:
-        return np.nan, 0.0
+        return np.nan, 0.0, 1.0
 
     rmode = modes[0]
     assert rmode.k.imag > 1e-9
     minfos = [
         newtils.NewtonModeInfo(
             k=rmode.k.real,
-            s=0.1,
+            s=initial_s,
             re_array=rmode.array.real,
             im_array=rmode.array.imag,
             dof_at_maximum=rmode.dof_at_maximum,
@@ -179,7 +202,7 @@ def calculate_mode_and_intensity(system, D1: int, D2: int) -> tuple[float, float
         newton_operators[1].L,
         newton_operators[1].delta_x,
         newton_operators[1].initial_x,
-        fail_early=True,
+        fail_early=False,
     )
 
     # TODO make sure that the other mode is below the threshold
@@ -187,18 +210,19 @@ def calculate_mode_and_intensity(system, D1: int, D2: int) -> tuple[float, float
     fac = refined_modes[0].k.real / (2 * np.pi)  # f*a/c
 
     # calculate the intensity of the mode at the two outer edges of the cavity
-    return fac, np.sum(abs(system.evaluator(refined_modes[0])) ** 2)
+    return fac, np.sum(abs(system.evaluator(refined_modes[0])) ** 2), refined_modes[0].s
 
 
-def test_single_mode_pump_trajectory(system):
+def test_single_mode_pump_trajectory_D1_0p85(system):
     fixed_D1 = 0.85
-    fac, intens = calculate_mode_and_intensity(system, fixed_D1, 0.2 * fixed_D1)
+    fac, intens, s = calculate_mode_and_intensity(system, fixed_D1, 0.2 * fixed_D1, 0.1)
 
     # intens/2. is very close to the expected Pout value from the paper (=0.0625)
     # The factor 2 is not clear
     correction_factor = 2.0
     assert intens / correction_factor == pytest.approx(0.06260821514272946, rel=1e-4)
     assert fac == pytest.approx(1.0013936748532917, rel=1e-4)
+    assert s == pytest.approx(0.33704142485274613, rel=1e-4)
 
     figs2a, ax1 = plt.subplots()
     ax1.plot(rdm.figs2a_data[:, 0], rdm.figs2a_data[:, 1], "x", label="paper reference")
@@ -208,24 +232,95 @@ def test_single_mode_pump_trajectory(system):
 
     all_intensitites = []
     all_facs = []
+    all_s = []
     ratios = np.linspace(0.2, 0.9, 32)
+    prev_s = 0.5
     for d0ratio in ratios:
-        fac, intens = calculate_mode_and_intensity(system, fixed_D1, d0ratio * fixed_D1)
+        log.info(f"################ {d0ratio=} ###################")
+        fac, intens, s = calculate_mode_and_intensity(
+            system, fixed_D1, d0ratio * fixed_D1, prev_s
+        )
+        prev_s = s
         all_intensitites.append(intens)
         all_facs.append(fac)
+        all_s.append(s)
 
-    ax1.plot(ratios, np.array(all_intensitites) / correction_factor, "-")
+    ax1.plot(
+        ratios, np.array(all_intensitites) / correction_factor, "-x", label="saltx"
+    )
     ax1.grid(True)
     ax1.set_xlabel("D2/D1")
     ax1.set_ylabel("Mode Intensity")
     ax1.axvline(x=D2D1_shutoff)
     ax1.axvline(x=D2D1_turnon)
+    ax1.legend()
 
-    ax2.plot(ratios, all_facs, "-")
+    ax2.plot(ratios, all_facs, "-", label="saltx")
     ax1.grid(True)
     ax2.set_xlabel("D2/D1")
     ax2.set_ylabel("f*a/c")
     ax2.axvline(x=D2D1_shutoff)
     ax2.axvline(x=D2D1_turnon)
+    ax2.legend()
+
+    plt.show()
+
+
+def test_single_mode_pump_trajectory_D1_0p95(system):
+    fixed_D1 = 0.95
+    fac, intens, s = calculate_mode_and_intensity(
+        system, fixed_D1, 0.2 * fixed_D1, initial_s=0.5
+    )
+
+    # The factor 2 is not clear
+    correction_factor = 2.0
+    assert intens / correction_factor == pytest.approx(0.16648743285132597, rel=1e-4)
+    assert fac == pytest.approx(1.0013936748532917, rel=1e-4)
+    assert s == pytest.approx(0.5481240251516201, rel=1e-4)
+
+    figs2a, ax1 = plt.subplots()
+    ax1.plot(
+        rdm.figs1_intensity_single_mode_data[:, 0],
+        rdm.figs1_intensity_single_mode_data[:, 1],
+        "x",
+        label="paper reference",
+    )
+
+    figs2b, ax2 = plt.subplots()
+    ax2.plot(
+        rdm.figs1_freq_mode1_data[:, 0],
+        rdm.figs1_freq_mode1_data[:, 1],
+        "x",
+        label="paper reference",
+    )
+
+    all_intensitites = []
+    all_facs = []
+    all_s = []
+    prev_s = 0.5
+    ratios = np.linspace(0.2, 0.8, 32)
+    for d0ratio in ratios:
+        log.info(f"################ {d0ratio=} ###################")
+        fac, intens, s = calculate_mode_and_intensity(
+            system, fixed_D1, d0ratio * fixed_D1, prev_s
+        )
+        prev_s = s
+        all_intensitites.append(intens)
+        all_facs.append(fac)
+        all_s.append(s)
+
+    ax1.plot(
+        ratios, np.array(all_intensitites) / correction_factor, "-x", label="saltx"
+    )
+    ax1.grid(True)
+    ax1.set_xlabel("D2/D1")
+    ax1.set_ylabel("Mode Intensity")
+    ax1.legend()
+
+    ax2.plot(ratios, all_facs, "-", label="saltx")
+    ax1.grid(True)
+    ax2.set_xlabel("D2/D1")
+    ax2.set_ylabel("f*a/c")
+    ax2.legend()
 
     plt.show()
